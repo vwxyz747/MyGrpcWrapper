5a5b486c4b431ff891dea671f38d8ec8864e3321
NickyChen <vwxyz7474@gmail.com>
2025-08-05 03:02:54 +0800

    fix:

--- Package.swift
+++ Package.swift
@@ -10,26 +10,28 @@ let package = Package(
     products: [
         .library(
             name: "MyGrpcWrapper",
-            type: .static, // 指定為靜態 library
+            type: .static,
             targets: ["MyGrpcWrapper"]
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/grpc/grpc-swift.git", exact: "1.21.0"),
+        .package(url: "https://github.com/grpc/grpc-swift.git", exact: "1.19.0"),
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.64.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", exact: "2.43.1")
     ],
     targets: [
-            .target(
-                name: "MyGrpcWrapper",
-                dependencies: [
-                    .product(name: "GRPC", package: "grpc-swift"),
-                    .product(name: "SwiftProtobuf", package: "swift-protobuf"),
-                    .product(name: "NIO", package: "swift-nio")
-                ],
-                swiftSettings: [
-                    .unsafeFlags(["-Xfrontend", "-disable-availability-checking"])
-                ]
-            )
-        ]
+        .target(
+            name: "MyGrpcWrapper",
+            dependencies: [
+                .product(name: "GRPC", package: "grpc-swift"),
+                .product(name: "SwiftProtobuf", package: "swift-protobuf"),
+                .product(name: "NIO", package: "swift-nio"),
+                .product(name: "NIOHTTP1", package: "swift-nio"),
+                .product(name: "NIOConcurrencyHelpers", package: "swift-nio")
+            ],
+            swiftSettings: [
+                .unsafeFlags(["-Xfrontend", "-disable-availability-checking"])
+            ]
+        )
+    ]
 )
